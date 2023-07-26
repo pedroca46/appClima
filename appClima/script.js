ce10c86a475671ece02b87d247cbd00528f735be
neon-ventura <123643824+neon-ventura@users.noreply.github.com>
2023-07-26 01:12:52 -0300

    Chamei os dados

--- appClima/script.js
+++ appClima/script.js
@@ -1,9 +1,14 @@
 const key = 'c6c69ec9374f2d92c09f8c8cf639bbe5'
 
+function colocarDadosNaTela(dados) {
+    console.log(dados)
+}
+
+
 async function buscarCidade(cidade) {
 
-    const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}`).then(resposta => resposta.json())
-    console.log(dados)
+    const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br`).then(resposta => resposta.json())
+    colocarDadosNaTela(dados)
    
 }
 
