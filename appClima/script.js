ef28ab4564fd86636de2f1812a037600870320bc
neon-ventura <123643824+neon-ventura@users.noreply.github.com>
2023-07-26 02:34:04 -0300

    js finalizado

--- appClima/script.js
+++ appClima/script.js
@@ -1,13 +1,16 @@
 const key = 'c6c69ec9374f2d92c09f8c8cf639bbe5'
 
 function colocarDadosNaTela(dados) {
-    console.log(dados)
+    document.getElementById('cidade').innerHTML = `Tempo em ${dados.name}`
+    document.getElementById('temp').innerHTML = Math.round(dados.main.temp) + '°C'
+    document.getElementById('previsao').innerHTML = dados.weather[0].description
+    document.getElementById('umidade').innerHTML = 'Umidade: ' + dados.main.humidity + '%'
 }
 
 
 async function buscarCidade(cidade) {
 
-    const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br`).then(resposta => resposta.json())
+    const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br&units=metric`).then(resposta => resposta.json())
     colocarDadosNaTela(dados)
    
 }
