8715e9aeabf4ca80a7e866d9d48707e25017bd33
pedroca46 <pedro.alves6491@gmail.com>
2023-07-25 23:39:52 -0300

    teste

---
+++ appClima/script.js
@@ -0,0 +1,11 @@
+
+
+
+async function buscarCidade(){
+    const dados = await fetch("")
+}
+
+function buscar_button(){
+    const cidade = document.querySelector(".cidade");
+    buscarCidade(cidade);
+}
