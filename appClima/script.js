2a9637379febf248ae2a412bfea2459c4dd53575
neon-ventura <123643824+neon-ventura@users.noreply.github.com>
2023-07-26 00:25:24 -0300

    Merge branch 'main' of https://github.com/pedroca46/appClima.git

--- appClima/script.js
+++ appClima/script.js
@@ -1,11 +1,13 @@
+const key = 'c6c69ec9374f2d92c09f8c8cf639bbe5'
 
+async function buscarCidade(cidade) {
 
-
-async function buscarCidade(){
-    const dados = await fetch("")
+    const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}`)
+   
 }
 
-function buscar_button(){
-    const cidade = document.querySelector(".cidade");
-    buscarCidade(cidade);
+
+function cliqueiNoBotao() {
+    const cidade = document.getElementById('cidade').value
+    buscarCidade(cidade)
 }
